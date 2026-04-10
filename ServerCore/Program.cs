4fb899cb202099ab6dabb1648197d5c3c5358f5f
김민성 <rlaalstjd6302@naver.com>
2026-04-11 03:56:33 +0900

    feat: thread creation examples

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -2,9 +2,45 @@
 {
     internal class Program
     {
+        static void PrintMessage()
+        {
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine($"Hello Thread! {i}");
+        }
+
+        static void PrintMessageWithState(object state)
+        {
+            for (int i = 0; i < 5; i++)
+                Console.WriteLine($"Hello Thread! {i}");
+        }
+
+        static void ThreadExample()
+        {
+            Thread t = new Thread(PrintMessage);
+            t.Name = "MyThread";
+            t.IsBackground = true;
+            t.Start();
+            t.Join();
+        }
+
+        static void ThreadPoolExample()
+        {
+            ThreadPool.QueueUserWorkItem(PrintMessageWithState);
+            Thread.Sleep(1000);
+        }
+
+        static void TaskExample()
+        {
+            Task t = new Task(PrintMessage);
+            t.Start();
+            t.Wait();
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            ThreadExample();
+            //ThreadPoolExample();
+            //TaskExample();
         }
     }
 }
