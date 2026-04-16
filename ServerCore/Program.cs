d16f12e054e6c725072273ba3c5e3d7e3307d927
김민성 <101568505+Minssuy99@users.noreply.github.com>
2026-04-16 15:46:09 +0900

    feat: Memory Barrier #1

--- ServerCore/Program.cs
+++ ServerCore/Program.cs
@@ -2,37 +2,48 @@
 {
     internal class Program
     {
+        static volatile int x = 0;
+        static volatile int y = 0;
+        static volatile int result1 = 0;
+        static volatile int result2 = 0;
+
+        static void Thread_1()
+        {
+            y = 1; // store y
+            Thread.MemoryBarrier();
+            result1 = x; // load x
+        }
+
+        static void Thread_2()
+        {
+            x = 1; // store x
+            Thread.MemoryBarrier();
+            result2 = y; // load y
+        }
+
         static void Main(string[] args)
         {
-            int[,] arr = new int[10000, 10000];
-
+            int count = 0;
+            while(true)
             {
-                long now = DateTime.Now.Ticks;
-                for(int y = 0; y < 10000; y++)
-                {
-                    for(int x = 0; x < 10000; x++)
-                    {
-                        arr[y, x] = 1;
-                    }
-                }
-                long end = DateTime.Now.Ticks;
-                Console.WriteLine($"(y, x) case's time : {end - now}");
-                // (y, x) case's time : 2329280
-            }
+                count++;
 
-            {
-                long now = DateTime.Now.Ticks;
-                for (int y = 0; y < 10000; y++)
+                x = y = result1 = result2 = 0;
+
+                Task t1 = new Task(Thread_1);
+                Task t2 = new Task(Thread_2);
+
+                t1.Start();
+                t2.Start();
+
+                Task.WaitAll(t1, t2);
+
+                if (result1 == 0 && result2 == 0)
                 {
-                    for (int x = 0; x < 10000; x++)
-                    {
-                        arr[x, y] = 1;
-                    }
+                    break;
                 }
-                long end = DateTime.Now.Ticks;
-                Console.WriteLine($"(x, y) case's time : {end - now}");
-                // (x, y) case 's time 4342085
             }
+            System.Console.WriteLine($"count : {count}");
         }
     }
 }
